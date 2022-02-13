2f70191b69ecb808976e0e42a3d1e37aa5848f3d
NovaMatthew <mnova412@gmail.com>
2022-02-12 21:28:02 -0500

    initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,15 @@
+//import java.util.Scanner;
+
+public class Calculator 
+{
+	public static void main(String[]args)
+	{
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter a number...");
+//		int input = sc.nextInt();
+//		System.out.println("Enter another number...");
+//		int input2 = sc.nextInt();
+//		System.out.println(output);
+		
+	}
+}
