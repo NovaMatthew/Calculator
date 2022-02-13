e9a67f8faf183e521ba127667793bcada2a33f65
NovaMatthew <mnova412@gmail.com>
2022-02-12 22:00:07 -0500

    conditional

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,10 +5,21 @@ public class Calculator
 	public static void main(String[]args)
 	{
 		Scanner sc = new Scanner(System.in);
+		Scanner bc = new Scanner(System.in);
 		System.out.println("Enter two whole numbers...");
 		int input = sc.nextInt();
 		int input2 = sc.nextInt();
-		int output = input + input2;
-	System.out.println(output);		
+		System.out.println("Would you like to add or subtract?");
+		String calculate = bc.nextLine();
+		if(calculate.equalsIgnoreCase("add"))
+		{
+			int add = input + input2;
+			System.out.println(add);		
+		}
+		else if (calculate.equalsIgnoreCase("subtract"))
+		{
+			int subtract = input - input2;
+			System.out.println(subtract);		
+		}
 	}
 }
